c1193778dff18b46743639c8facf183ee8582590
chhuonsrinie-blip <chhuonsrinie@gmail.com>
2026-01-18 14:23:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from urllib.parse import urlparse
 
 import requests
 import streamlit as st
-from bs4 import BeautifulSoup
-from PIL import Image, ImageDraw, ImageFont
+import json
+import zipfile
+from io import BytesIO
+from typing import List
 
 
 # ---------------------------
